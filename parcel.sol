41e6b6f7561e137538d9208ca4626df7bc09ec6c
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-12-13 18:59:57 +0530

    Update parcel.sol

--- parcel.sol
+++ parcel.sol
@@ -8,11 +8,17 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract ArtParceling is ERC1155 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
+    uint public totalProposal;
 
-    struct Artist {
-        uint ids;
+    event BidProposalCreated(uint indexed bidId, uint indexed parentId, uint proposedPrice, address indexed proposer);
+
+    
+    struct Art{
+        uint id;
         uint salePrice;
+        uint noOfParcel;
         uint pricePerParcel;
+        bool onSale;
         address artist;
     }
 
@@ -23,113 +29,180 @@ contract ArtParceling is ERC1155 {
 
     struct Parcel {
         uint parentId;
-        uint price;
+        uint parcelId;
+        uint parcelPrice;
         address owner;
+        bool isForSale;
     }
 
-    modifier onlyArtist(uint tokenId){
-        require(msg.sender==_tokenDetails[tokenId].artist,"only artist allowed");
+    struct Proposal{
+        uint parentId;
+        uint proposedPrice;
+        address Proposer;
+        bool status;
+    }
+
+    struct Voters{
+        uint parentId;
+        address voterAddress;
+        bool status;
+    }
+
+    modifier onlyArtist(uint parentId){
+        require(msg.sender==_artDetails[parentId].artist,"only artist allowed");
         _;
     }
 
-    mapping(uint256 => Artist) public _tokenDetails;
+    mapping(uint256 parentId=> Art) public _artDetails;
     mapping(uint256 => Investor) public _investorDetails;
-    mapping(uint256 => Parcel) public _parcelDetails;
+    mapping(uint256 parcelId=> Parcel) public _parcelDetails;
+    mapping(uint256 parentId => Proposal) public viewProposals;
 
     constructor() ERC1155("https://game.example/api/item/{id}.json") {}
 
-    function mintNFT(uint256 salePrice, uint256 noOfParcels, uint _pricePerParcel) external {
+
+/*
+1- mint parentNft
+2- next parentId should depend on noOfParcels of the token eg:parentId=0 noOfParcel =10=> next parentId= 11
+*/
+
+ function mintNFT(uint256 salePrice, uint256 noOfParcels, uint _pricePerParcel) external {
         require(salePrice > 0, "Please set some amount for your NFT");
         require(noOfParcels > 0, "Number of parcels must be greater than zero");
 
         uint256 parentId = _tokenIdCounter.current();
-
         _mint(msg.sender, parentId, 1, "");
 
-        _tokenDetails[parentId] = Artist({
-            ids: parentId,
+        _artDetails[parentId] = Art({
+            id: parentId,
             salePrice: salePrice,
+            noOfParcel: noOfParcels,
             pricePerParcel: _pricePerParcel,
+            onSale:true,
             artist: msg.sender
         });
 
-        // _parcelDetails[parentId] = Parcel({
-        //     // parentId: 0, 
-        //     price: _pricePerParcel,
-        //     owner: msg.sender
-        // });
-
-        for (uint256 i = 1; i <= noOfParcels; i++) {
-            _tokenIdCounter.increment();
-            uint256 tokenId = _tokenIdCounter.current();
-
-            _mint(msg.sender, tokenId, 1, "");
-
-            _tokenDetails[tokenId] = Artist({
-                ids: tokenId,
-                salePrice: salePrice,
-                pricePerParcel: _pricePerParcel,
-                artist: msg.sender
-            });
-
-            _parcelDetails[tokenId] = Parcel({
-                parentId: parentId,
-                price: _pricePerParcel,
-                owner: msg.sender
-            });
-        }
-    }
-//run for loop to set price 
-    // function setPriceForEachParcel(uint id, uint price) external onlyOwner {
-    //     require(_tokenDetails[id].artist != address(0), "Invalid NFT ID");
-    //     require(price > 0, "Price must be greater than zero");
-
-    //     _tokenDetails[id].pricePerParcel = price;
-    // }
-
-     function setPriceForMultipleParcels(uint[] memory ids, uint[] memory prices) external onlyArtist(ids[0]) {
-        require(ids.length == prices.length, "length mismatch");
-
-        for (uint i = 0; i < ids.length; i++) {
-            uint id = ids[i];
-            uint price = prices[i];
-
-            require(_tokenDetails[id].artist != address(0), "Invalid NFT ID");
-            require(price > 0, "Price must be greater than zero");
-
-            _tokenDetails[id].pricePerParcel = price;
-        }
+       for (uint256 i = 0; i < noOfParcels + 1; i++) {
+        _tokenIdCounter.increment();
     }
 
-    function makeInvestmentOffer(uint id, uint price) external payable {
-    require(id > 0, "Enter valid id"); //changes here
-    require(_tokenDetails[id].pricePerParcel == price, "Pay the exact amount");
+    }
    
-    require(_parcelDetails[id].parentId == 0, "Parent token cannot be purchased by investors");
+   /*
+   1- set price for each child nft
+   */
+  function setPriceForMultipleParcels(uint256 parentId, uint256[] memory prices) external onlyArtist(parentId) {
+    require(prices.length == _artDetails[parentId].noOfParcel, "Length mismatch");
 
-    // Transfer the NFT to investor
-    address nftOwner = _tokenDetails[id].artist;
-    _safeTransferFrom(nftOwner, msg.sender, id, 1, "");
+    for (uint256 i = 0; i < prices.length; i++) {
+        uint256 childTokenId = parentId + i + 1; 
 
-    // Transfer the amount to NFT owner
-    payable(nftOwner).transfer(price);
+        _mint(msg.sender, childTokenId, 1, "");
 
-    // Update investor details
-    require(_investorDetails[id].investor == address(0), "Investor already exists for this NFT");
+        _parcelDetails[childTokenId] = Parcel({
+            parentId: parentId,
+            parcelId: childTokenId,
+            parcelPrice: prices[i],
+            owner: msg.sender,
+            isForSale: true
+        });
+    }
+}
 
-    _investorDetails[id] = Investor({
-        ids: id,
+/*
+This method enables investors to invest in Art Parcel(s). 
+Once the investor completes the payment, the corresponding parcel(s) will be transferred to their wallet as an NFT.
+ This NFT will be locked and cannot be transferred to anyone else, ensuring the investor's ownership and security of the assets they have invested in
+
+ Input Parameters:Proposal Id,Parcel Id(multiple),Parcel Amount,Wallet Address,
+Output Parameters:Investor Detail,Proposal Id,Investor Id,Investor Address,Parcel Contract Address,Parcel Id(multiple),Parcel Amount
+
+Action
+Transfer Admin Commision into Parcel Amount,
+Transfer Remaining Parcel Sub NFT Amount to Proposer,
+Smart Contract transfer Sub NFT tokens to Investor,
+Store Preferred Investor Detail,
+*/
+//NOTE: need to be tested
+
+function makeInvestment(uint parentId, uint parcelId) external payable { //make parcelId [] 
+    require(parentId >= 0 && parentId <= _tokenIdCounter.current(), "Invalid parentId");
+    require(parcelId > parentId && parcelId <= parentId + _artDetails[parentId].noOfParcel, "Invalid parcelId");
+    require(_artDetails[parentId].onSale, "Parent NFT is not on sale");
+    require(_parcelDetails[parcelId].isForSale,"parcel already purchased");
+    require(_parcelDetails[parcelId].owner == _artDetails[parentId].artist, "Invalid parcel ownership");
+    require(msg.value == _parcelDetails[parcelId].parcelPrice, "Incorrect payment amount");
+
+    // // Ensure that the contract has approval to transfer the token on behalf of the owner
+    // setApprovalForAll(_artDetails[parentId].artist, true);
+
+    // Transfer ownership of the parcel NFT to the investor
+    safeTransferFrom(_artDetails[parentId].artist, msg.sender, parcelId, 1, "");
+
+    // // Remove approval after the transfer is complete (optional, depending on your requirements)
+    // setApprovalForAll(_artDetails[parentId].artist, false);
+
+    // Lock the transferred NFT (make it non-transferable)
+    _parcelDetails[parcelId].owner = msg.sender;
+     _parcelDetails[parcelId].isForSale= false;
+
+    _investorDetails[parcelId] = Investor({
+        ids: parcelId,
         investor: msg.sender
     });
+
 }
 
 
-    function createBid (uint id) external {
+/*Create Bid Proposal-This method enables a artist to create a bid proposal for a Parent NFT. 
+ The proposal will be sent to all Sub NFT holders for voting purposes.
+ The voting process allows for collective decision-making by the community of Sub NFT holders to approve or reject the bid proposal and reserve price.
+
+ Input Parameters: Proposal Id,Amount
+ Output Parameters:Bid Detail,Bid Id,Proposal Id, Bid Amount,Bid Winner
+ Use by :Admin
+
+Action		
+Store Bid Detail for proposal		
+
+*/
+ function createBidProposal(uint parentId, uint price) public onlyArtist(parentId) {
+    require(parentId >= 0 && parentId <= _tokenIdCounter.current(), "Invalid parentId");
+    require(_artDetails[parentId].onSale, "Parent NFT is not on sale");
+
+    totalProposal++;
+    viewProposals[parentId] = Proposal({
+        parentId: parentId,
+        proposedPrice: price,
+        Proposer: msg.sender,
+        status: false
+    });
+
+    emit BidProposalCreated(totalProposal, parentId, price, msg.sender);
+}
+
+/*
+Voting
+Parcel holders have the voting power to approve or reject bid requests for the Master NFT. 
+If the majority of parcel holders accept the proposal, the Master NFT will be sold to the selected bidder. 
+Parcel holders will receive their profits, and the corresponding Master NFT will be transferred to the bidder. 
+Sub NFTs (parcels) will be burned, ensuring a seamless and transparent transaction process for all parties involved
+	
+Input Parameters:Proposal Id,Sub-NFT Holder  Address,Vote Status		
+Output Parameters:Vote Id,Proposal Id,Vote Status,Sub-NFT Holder Address
+Use by: Token Holders
+		
+Action		
+Store Master NFT Biding Voting Detail		
+
+*/
+    function votingOnBidProposal() external{
 
     }
-
-    function viewArtDetails(uint256 tokenId) public view returns(Artist memory) {
-        return _tokenDetails[tokenId];
+    
+    
+    function viewArtDetail(uint256 parentId) public view returns(Art memory) {
+        return _artDetails[parentId];
     }
 
     function viewParcelDetails(uint256 tokenId) public view returns(Parcel memory){
@@ -140,8 +213,7 @@ contract ArtParceling is ERC1155 {
         return _investorDetails[tokenId];
     }
 
-} 
+ } 
 //10000000000000000000
 //1000000000000000000
 
-//condition for parentId
