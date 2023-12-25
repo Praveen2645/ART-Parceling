67026331b8d79fee91851163f9a381b80fa52478
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-12-25 15:21:16 +0530

    Update parcelToken.sol

--- parcelToken.sol
+++ parcelToken.sol
@@ -1,9 +1,9 @@
-// contracts/GameItem.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
 
 contract ParcelToken is ERC721URIStorage {
     using Counters for Counters.Counter;
@@ -18,16 +18,23 @@ contract ParcelToken is ERC721URIStorage {
         }
     }
 
+    function approveTransfer(address to, uint256 tokenId) external {
+        // Approve the specified address to transfer the token
+        approve(to, tokenId);
+    }
+
     function TotalSupply() external view returns (uint256) {
         return _tokenIds.current();
     }
 
-    function getOwner() public view returns (address) {
-        require(_tokenIds.current() > 0, "No tokens minted yet");
+    // function getOwner() public view returns (address) {
+    //     require(_tokenIds.current() > 0, "No tokens minted yet");
 
-        uint256 latestTokenId = _tokenIds.current() - 1;
-        address owner = ownerOf(latestTokenId);
+    //     uint256 latestTokenId = _tokenIds.current() - 1;
+    //     address owner = ownerOf(latestTokenId);
 
-        return owner;
-    }
+    //     return owner;
+    // }
+
+    function burnTokens() public{}
 }
